3055f09fa376d108f628c92a95eae3b97033cc86
Hans5958 <hans5958@outlook.com>
2022-01-09 11:08:12 +0700

    Make icons working, use timestamps for more accuracy

--- components/Timer.vue
+++ components/Timer.vue
@@ -4,26 +4,46 @@
 			<p class='text-2xl cursor-pointer' @click='rename'>{{ title }}</p>
 		</div>
 		<div class="display mb-4">
-			<p class='text-6xl cursor-pointer' @click='retime'>{{ display }}</p>
+			<p class='text-6xl cursor-pointer' @click='retime'>{{ display }}<span class='text-xl text-gray-500'>.{{ displayms }}</span></p>
 		</div>
 		<div class="controls">
-			<button class='bg-green-600 hover:bg-green-700 text-white rounded p-2' type='button' @click='start' v-if='!isStarted'>Start</button>
-			<button class='bg-yellow-600 hover:bg-yellow-700 text-white rounded p-2' type='button' @click='pause' v-if='isStarted'>Pause</button>
-			<button class='bg-red-600 hover:bg-red-700 text-white rounded p-2' type='button' @click='stop'>Stop</button>
-			<button class='bg-blue-700 hover:bg-blue-800 text-white rounded p-2' type='button' @click='countup' v-if='!countingUp'>Count Up</button>
-			<button class='bg-blue-700 hover:bg-blue-800 text-white rounded p-2' type='button' @click='countdown' v-if='countingUp'>Count Down</button>
-			<button class='bg-red-700 hover:bg-red-800 text-white rounded p-2' type='button' @click='remove'>Remove</button>	
+			<button class='bg-green-600 hover:bg-green-700 text-white rounded p-2' type='button' @click='start' v-if='!isStarted'>
+				<client-only><Icon icon="clarity:play-solid" :inline="true" /></client-only>
+				Start
+			</button>
+			<button class='bg-yellow-600 hover:bg-yellow-700 text-white rounded p-2' type='button' @click='pause' v-if='isStarted'>
+				<client-only><Icon icon="clarity:pause-solid" :inline="true" /></client-only>
+				Pause
+			</button>
+			<button class='bg-red-600 hover:bg-red-700 text-white rounded p-2' type='button' @click='stop'>
+				<client-only><Icon icon="clarity:stop-solid" :inline="true" /></client-only>
+				Stop
+			</button>
+			<button class='bg-blue-700 hover:bg-blue-800 text-white rounded p-2' type='button' @click='countup' v-if='!countingUp'>
+				<client-only><Icon icon="clarity:plus-circle-solid" :inline="true" /></client-only>
+				Count Up
+			</button>
+			<button class='bg-blue-700 hover:bg-blue-800 text-white rounded p-2' type='button' @click='countdown' v-if='countingUp'>
+				<client-only><Icon icon="clarity:minus-circle-solid" :inline="true" /></client-only>
+				Count Down
+			</button>
+			<button class='bg-red-700 hover:bg-red-800 text-white rounded p-2' type='button' @click='remove'>
+				<client-only><Icon icon="clarity:trash-solid" :inline="true" /></client-only>
+				Remove
+			</button>	
 		</div>
 	</div>
 </template>
 
 <style>
-	.iconify--el {
+	svg {
 		display: inline
 	}
 </style>
 
 <script>
+import { Icon } from '@iconify/vue2';
+
 export default {
 	props: [
 		'id'
@@ -36,42 +56,63 @@ export default {
 			m: 0,
 			s: 0,
 			isStarted: false,
-			totalSeconds: 0,
 			interval: () => {},
 			title: 'Timer',
 			countingUp: true,
-			default: 0
+			default: 0,
+			targetTimestamp: 0,
+			pausedDifference: 0,
+			displayms: '000'
 		}
 	},
 	methods: {
 		start() {
+			let nowTimestamp = Date.now()
+			let targetTime = this.default
+			if (this.pausedDifference) {
+				targetTime = this.pausedDifference
+				this.pausedDifference = 0
+			}
+			if (this.countingUp) this.targetTimestamp = nowTimestamp - targetTime
+			else this.targetTimestamp = nowTimestamp + targetTime
 			this.interval = setInterval(() => {
-				if (this.countingUp) this.totalSeconds += 1
-				else {
-					if (this.totalSeconds) this.totalSeconds -= 1
-				}
-				const s = this.totalSeconds
-				this.updateDisplay()
-			}, 1000)
+				if (!this.countingUp && this.targetTimestamp - Date.now() < 1) {
+					this.pause()
+					this.updateDisplay(0)
+				} else this.updateDisplay()
+			}, 10)
 			this.isStarted = true
 		},
 		pause() {
+			let nowTimestamp = Date.now()
+			this.pausedDifference = Math.abs(this.targetTimestamp - nowTimestamp)
 			clearInterval(this.interval)
 			this.updateDisplay()
 			this.isStarted = false
 		},
 		stop() {
-			this.pause()
-			this.totalSeconds = this.default
+			if (this.isStarted) this.pause()
+			this.reset()
 			this.updateDisplay()
-			this.isStarted = false
+		},
+		reset() {
+			let nowTimestamp = Date.now()
+			if (this.countingUp) this.targetTimestamp = nowTimestamp - this.default
+			else this.targetTimestamp = nowTimestamp + this.default
+			this.pausedDifference = 0
 		},
 		remove() {
 			this.$emit('remove', this.id)
 		},
-		updateDisplay() {
-			const s = this.totalSeconds
-			this.s = s % 60
+		updateDisplay(targetTime) {
+			let nowTimestamp = Date.now()
+			if (targetTime !== undefined) {
+				if (this.countingUp) this.targetTimestamp = nowTimestamp - targetTime
+				else this.targetTimestamp = nowTimestamp + targetTime
+			}
+			const s = Math.abs(Date.now() - this.targetTimestamp) / 1000
+			this.ms = Math.floor((s % 1) * 1000)
+			this.s = Math.floor(s % 60)
 			this.m = Math.floor(s/60) % 60
 			this.h = Math.floor(s/3600) % 24
 			this.d = Math.floor(s/86400)
@@ -80,6 +121,7 @@ export default {
 			display = `${(this.h+'').length === 1 ? 0 : ''}${this.h}:` + display
 			if (this.d) display = `${(this.d+'').length === 1 ? 0 : ''}${this.d}:` + display
 			this.display = display
+			this.displayms = ("000" + this.ms).slice(-3)
 			// console.log(display)
 		},
 		rename() {
@@ -87,18 +129,27 @@ export default {
 		},
 		countup() {
 			this.countingUp = true
+			let nowTimestamp = Date.now()
+			let difference = Math.abs(this.targetTimestamp - nowTimestamp)
+			this.targetTimestamp = nowTimestamp - difference
 		},
 		countdown() {
 			this.countingUp = false
+			let nowTimestamp = Date.now()
+			let difference = Math.abs(this.targetTimestamp - nowTimestamp)
+			this.targetTimestamp = nowTimestamp + difference
 		},
 		retime() {
 			let input = prompt('Enter the new time in seconds.')
-			if (!Number.isNaN(Number(input))) {
-				this.totalSeconds = Number(input)
-				this.default = Number(input)
-				this.updateDisplay()
+			if (!Number.isNaN(Number(input)) && input > -1 ) {
+				this.default = Number(input) * 1000
+				this.updateDisplay(this.default)
+				if (this.pausedDifference) this.pausedDifference += this.default
 			}
 		},
+	},
+	components: {
+		Icon,
 	},
 }
 </script>
