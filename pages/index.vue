cf92b7ef414f896e80bc3ac1b4df1db0bec50399
Hans5958 <hans5958@outlook.com>
2022-01-09 12:16:44 +0700

    Rename to "Add another timer"

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
 				<Timer v-for="field in fields" :id="field.id" :key="field.id" @remove='removeTimer'/>
 				<div class='bg-white p-2 h-auto w-auto rounded group cursor-pointer hover:shadow-2xl transition ease-in-out' @click='addTimer'>
 					<div class='border-2 border-gray-300 border-dashed focus:border-solid group-hover:border-gray-700 h-full w-full rounded flex items-center justify-center transition ease-in-out'>
-						<p class='group-hover:text-gray-900 text-gray-500 text-lg my-10 transition ease-in-out'>Add more timers</p>
+						<p class='group-hover:text-gray-900 text-gray-500 text-lg my-10 transition ease-in-out'>Add another timer</p>
 					</div>
 				</div>
 			</div>
