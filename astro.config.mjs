9d686f47169c2469b4a62ec4555f183ff4761f1c
Hans5958 <hans5958@outlook.com>
2024-01-27 19:26:22 +0700

    Update dependencies and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [vue(), tailwind()],
+  integrations: [vue(), tailwind(), icon()],
   base: '/hyper-flexible-timer'
 });
