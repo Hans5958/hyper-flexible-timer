ad13a10d0a1d4d75500186300d37ab4e18f28458
Hans5958 <hans5958@outlook.com>
2025-05-08 23:31:56 +0700

    Add more meta tags

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Hyper Flexible Timer';
+export const SITE_AUTHOR = 'Hans5958'
+export const SITE_TWITTER = 'Hans5958'
+export const SITE_MASTODON = '@Hans5958@mastodon.social'
